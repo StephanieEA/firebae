3bacf0289e04c4072dd6c0799cb300bc836bf687
Stephanie Andrews <seandrews11@gmail.com>
2017-01-10 11:23:17 -0700

    add default state tests

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import moment from 'moment';
 import { shallow, mount, render } from 'enzyme';
 import { assert, expect } from 'chai';
 import sinon from 'sinon';
@@ -26,6 +26,35 @@ describe('Application', () => {
     const wrapper = shallow(<Application />);
     expect(wrapper.find('Match')).to.have.length(3);
   });
+
+  it('has a state of loadArray equal to an empty array', () => {
+    const wrapper = shallow(<Application />)
+    expect(wrapper.state().loadArray).to.deep.equal([])
+  })
+
+  it('has a state of monthlyIncome equal to null', () => {
+    const wrapper = shallow(<Application />)
+    expect(wrapper.state().monthlyIncome).to.deep.equal(null)
+  })
+
+  it('has a state of budgets with default key vale pairs', () => {
+    const wrapper = shallow(<Application />)
+    expect(wrapper.state().budgets).to.deep.equal({
+      draftBudgetItem: '',
+      draftItemValue: '',
+      itemTime: moment(Date.now()).format('MM/D, h:mm a'),
+    })
+  })
+
+  it('has a default state of user equal to null', () => {
+    const wrapper = shallow(<Application />)
+    expect(wrapper.state().user).to.deep.equal(null)
+  })
+
+  it('has a default state of draftMonthly equal to ""', () => {
+    const wrapper = shallow(<Application />)
+    expect(wrapper.state().draftMonthly).to.deep.equal('')
+  })
 
   it('calls componentDidMount', () => {
     sinon.spy(Application.prototype, 'componentDidMount');
