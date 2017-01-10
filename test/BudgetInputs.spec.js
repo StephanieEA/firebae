062952c35ce1098ed0c53251b597bf56c2412590
Stephanie Andrews <seandrews11@gmail.com>
2017-01-09 17:34:15 -0700

    another test...

--- test/BudgetInputs.spec.js
+++ test/BudgetInputs.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import sinon from 'sinon';
 import { shallow, mount, render } from 'enzyme';
 import { assert, expect } from 'chai';
 
@@ -18,8 +18,27 @@ describe('BudgetInputs', () => {
 
   it('has a td input with a placeholder rent', () => {
     const wrapper = shallow(<BudgetInputs />);
-    const 
-    expect(wrapper.find('td')).to.have.length(3);
+    const input = wrapper.find('.tdInput').first();
+    expect(input.props().placeholder).to.equal('rent');
   })
+
+  it('has a td input with a placeholder $800', () => {
+    const wrapper = shallow(<BudgetInputs />);
+    const input = wrapper.find('.tdInput').at(1);
+    expect(input.props().placeholder).to.equal('$800');
+  })
+
+  // it('updates the budgetItem', () => {
+  //   const updateItem = sinon.spy();
+  //   const wrapper = shallow(<BudgetInputs onChange={updateItem}/>);
+  //   const inputField = wrapper.find('input').first();
+  //   const event1 = { target: { value: 'dogs' } };
+  //   const event2 = { target: { value: 'frogs' } };
+  //   assert.equal(updateItem.calledOnce, false);
+  //   inputField.simulate('change', event1);
+  //   assert.equal(updateItem.calledOnce, true);
+  //   inputField.simulate('change', event2);
+  //   assert.equal(updateItem.calledTwice, true);
+  // });
 
 });
